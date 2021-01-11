f82934a6eea993f75e0f0f63afc1d38b9a0d5829
afthamafer <37636774+afthamafer@users.noreply.github.com>
2021-01-11 11:10:52 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#2571d7',
+          300: '#888883',
+          
+        }
+      },
+      fontFamily: {
+        body: ['Roboto']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
